9420a020a9822bd240304fa70fc3ec24aea6c30f
topzip <topzip2142@yandex.ru>
2026-08-05 11:23:03 +0300

    fix: githubtest

--- gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -2,12 +2,11 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ShareItGateway {
-    public static void main(String[] args) {
-        SpringApplication.run(ShareItGateway.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ShareItGateway.class, args);
+	}
 
 }
--- gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.booking;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -7,10 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-import ru.practicum.shareit.booking.dto.NewBookingRequest;
-import ru.practicum.shareit.client.BaseClient;
 
-import java.util.Map;
+import ru.practicum.shareit.booking.dto.BookItemRequestDto;
+import ru.practicum.shareit.booking.dto.BookingState;
+import ru.practicum.shareit.client.BaseClient;
 
 @Service
 public class BookingClient extends BaseClient {
@@ -26,43 +28,21 @@ public class BookingClient extends BaseClient {
         );
     }
 
+    public ResponseEntity<Object> getBookings(long userId, BookingState state, Integer from, Integer size) {
+        Map<String, Object> parameters = Map.of(
+                "state", state.name(),
+                "from", from,
+                "size", size
+        );
+        return get("?state={state}&from={from}&size={size}", userId, parameters);
+    }
+
+
+    public ResponseEntity<Object> bookItem(long userId, BookItemRequestDto requestDto) {
+        return post("", userId, requestDto);
+    }
+
     public ResponseEntity<Object> getBooking(long userId, Long bookingId) {
         return get("/" + bookingId, userId);
     }
-
-    public ResponseEntity<Object> createBooking(NewBookingRequest booking, long userId) {
-        return post("", userId, booking);
-    }
-
-    public ResponseEntity<Object> approveBooking(long userId, long bookingId, boolean approved) {
-        String path = "/" + bookingId + "?approved={approved}";
-
-        Map<String, Object> parameters = Map.of(
-                "approved", approved
-        );
-
-        return patch(path, userId, parameters, null);
-    }
-
-    public ResponseEntity<Object> getAllBookings(long userId, String state) {
-        String path = "?state={state}";
-
-        Map<String, Object> parameters = Map.of(
-                "state", state
-        );
-
-        return get(path, userId, parameters);
-    }
-
-    public ResponseEntity<Object> getOwnerBookings(long userId, String state) {
-        String path = "/owner?state={state}";
-
-        Map<String, Object> parameters = Map.of(
-                "state", state
-        );
-
-        return get(path, userId, parameters);
-    }
-
-
 }
